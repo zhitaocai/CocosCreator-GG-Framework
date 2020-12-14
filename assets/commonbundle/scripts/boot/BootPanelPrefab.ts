b3f19c48157720df9cd176123cf004fc373e0a03
caizhitao <czt.saisam@gmail.com>
2020-12-14 22:03:39 +0800

    完善 boot

--- assets/commonbundle/scripts/boot/BootPanelPrefab.ts
+++ assets/commonbundle/scripts/boot/BootPanelPrefab.ts
@@ -1,12 +1,13 @@
 import { Panels } from "../../../mainbundle/scripts/configs/Panels";
 import { gg } from "../../../scripts/framework/gg";
 import { PanelComponent, PanelHideOption, PanelShowOption } from "../../../scripts/framework/lib/router/PanelComponent";
-import { LoadingPanelShowArgs } from "../popwindow/LoadingPanelPrefab";
-import { ToastPanelShowArgs } from "../popwindow/ToastPanelPrefab";
+import GameSettingModule from "../setting/GameSettingModule";
 
 const { ccclass, property } = cc._decorator;
 
 /**
+ * 启动页面板
+ *
  * @author caizhitao
  * @created 2020-12-14 21:29:55
  */
@@ -16,75 +17,60 @@ export default class BootPanelPrefab extends PanelComponent {
         type: cc.ProgressBar,
         tooltip: "加载进度",
     })
-    loadingProgressBar: cc.Node = null;
+    loadingProgressBar: cc.ProgressBar = null;
 
     @property({
         type: cc.Label,
         tooltip: "加载文本",
     })
-    loadingLabel: cc.Node = null;
+    loadingLabel: cc.Label = null;
 
     show(option: PanelShowOption): void {
         option.onShowed();
-        //     // 默认播放loading动画
-        //     this._playPanelShowAnim(() => {
-        //         option.onShowed();
-        //     });
+        this._initGame();
     }
 
     hide(option: PanelHideOption): void {
         option.onHided();
-        // this._playPanelHideAnim(() => {
-        //     option.onHided();
-        // });
     }
 
-    // private async _initGame() {
-    //     // 初始化游戏设置
-    //     GameSettingModule.initSettingConfig();
+    private async _initGame() {
+        // 初始化游戏设置
+        this._onLoadProgressChanged(0, "加载游戏设置...");
+        GameSettingModule.initSettingConfig();
 
-    //     // 提前加载通用弹窗面板
-    //     await gg.panelRouter.loadAsync(Panels.loadingPanel);
-    //     await gg.panelRouter.loadAsync(Panels.toastPanel);
+        // 提前加载通用弹窗面板
+        this._onLoadProgressChanged(0.1, "加载游戏资源...");
+        await gg.panelRouter.loadAsync(Panels.loadingPanel);
 
-    //     // 加载游戏设置面板
-    //     await gg.panelRouter.loadAsync(Panels.gameSettingPanel);
+        this._onLoadProgressChanged(0.2, "加载游戏资源...");
+        await gg.panelRouter.loadAsync(Panels.loadingPanel);
 
-    //     // 加载游戏主面板
-    //     await gg.panelRouter.loadAsync(Panels.gamePanel);
+        // 加载游戏设置面板
+        this._onLoadProgressChanged(0.3, "加载游戏资源...");
+        await gg.panelRouter.loadAsync(Panels.gameSettingPanel);
 
-    //     // 打开主面板
-    //     gg.panelRouter.show({
-    //         panelConfig: Panels.gamePanel,
-    //     });
-    // }
+        // 加载游戏主面板
+        this._onLoadProgressChanged(0.4, "加载游戏资源...");
+        await gg.panelRouter.loadAsync(Panels.gamePanel);
 
-    onShowToastPanelBtnClick() {
-        //   // 提前加载 Pop 弹窗面板
-        //   await gg.panelRouter.loadAsync(Panels.loadingPanel);
-        //   await gg.panelRouter.loadAsync(Panels.toastPanel);
+        // 打开主面板
+        this._onLoadProgressChanged(1.0);
         gg.panelRouter.show({
-            panelConfig: Panels.toastPanel,
-            data: <ToastPanelShowArgs>{
-                text: "短Toast测试",
-            },
+            panelConfig: Panels.gamePanel,
         });
     }
 
-    onShowLoadingPanelBtnClick() {
-        // 打开面板弹窗
-        gg.panelRouter.show({
-            panelConfig: Panels.loadingPanel,
-            data: <LoadingPanelShowArgs>{
-                playShowAnim: true,
-            },
-            onShowed: () => {},
-        });
-    }
-
-    onShowGameSettingPanelBtnClick() {
-        gg.panelRouter.show({
-            panelConfig: Panels.gameSettingPanel,
-        });
+    /**
+     * 加载进度更新
+     *
+     * @param pb 加载进度 [0, 1]
+     * @param msg 加载描述信息
+     */
+    private _onLoadProgressChanged(pb: number, msg: string = null) {
+        this.loadingProgressBar.progress = pb;
+        if (msg) {
+            this.loadingLabel.string = msg;
+        }
     }
 }
